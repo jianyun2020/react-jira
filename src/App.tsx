2ede3a66eff9238510e3c598588eac4b0e6e141b
jianyun2020 <sujianyun2020@163.com>
2022-03-20 14:17:08 +0800

    feat: loginScreen组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
-import { ProjectListScreen } from "./screens/project-list";
+// import { ProjectListScreen } from "screens/project-list";
+import { LoginScreen } from "screens/login";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <ProjectListScreen />
+        {/* <ProjectListScreen /> */}
+        <LoginScreen />
       </div>
     );
   }
