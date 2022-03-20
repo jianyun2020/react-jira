74c4e76347a3387761e27f023e7d069a4f9932f4
jianyun2020 <sujianyun2020@163.com>
2022-03-20 13:17:02 +0800

    chore: any->unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 // 判断value为0的特殊情况
-export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 // 在一个函数里，改变传入的对象本身是不好的，防止原对象被污染
 export const cleanObject = (object: object) => {
@@ -27,8 +27,9 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
+// 后面用泛型进行类型约束，暂时用anys
 // useDebounce  delay?:number 要么不传delay，要么传number
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = (value: unknown, delay?: number): any => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
