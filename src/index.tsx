22d61d465cd4e0f030f2acc270cea43d4283aad6
jianyun2020 <sujianyun2020@163.com>
2022-03-20 14:35:42 +0800

    chore: 安装devtool

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { loadDevTools } from "jira-dev-tool";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+loadDevTools(() => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
