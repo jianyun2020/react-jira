8449462773e80d9f4a21f1546657fcb5f4027e69
jianyun2020 <sujianyun2020@163.com>
2022-03-23 16:38:52 +0800

    feat: 用grid和flex布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { Button } from "antd";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
@@ -5,11 +6,71 @@ import { ProjectListScreen } from "screens/project-list";
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <Button type="primary" danger onClick={logout}>
-        登出
-      </Button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h2>logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <Button type="primary" danger onClick={logout}>
+            登出
+          </Button>
+        </HeaderRight>
+      </Header>
+      <Nav>nav</Nav>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+      <Aside>aside</Aside>
+      <Footer>footer</Footer>
+    </Container>
   );
 };
+
+/**
+ * Gird和Flex各自的应用场景
+ * 1. 要考虑，是一维布局还是二维布局
+ * 一般来说，一维布局用flex，二维布局用gird
+ * 2. 是从内容出发还是从布局出发？
+ * 从内容出发（用flex）：先有一组内容（数量一般不固定),然后希望它们均匀的分布在容器中，由内容自己的大小决定占据的空间
+ * 从布局出发（用grid）：先规划网格（数量一般比较固定），然后再把元素往里填充
+ */
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-areas:
+    "header header header"
+    "nav main aside"
+    "footer footer footer";
+  height: 100vh;
+`;
+
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  grid-area: main;
+`;
+const Nav = styled.nav`
+  grid-area: nav;
+`;
+const Aside = styled.aside`
+  grid-area: aside;
+`;
+const Footer = styled.footer`
+  grid-area: footer;
+`;
