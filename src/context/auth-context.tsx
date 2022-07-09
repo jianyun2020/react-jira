c5de39a9741e534c7658b5046100b62430d8fa27
jianyun2020 <sujianyun2020@163.com>
2022-07-10 06:42:17 +0800

    feat: 添加登录状态维持

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,6 +1,8 @@
 import * as auth from "auth-provider";
 import React, { createContext, ReactNode, useContext, useState } from "react";
 import { User } from "screens/project-list/search-panel";
+import { useMount } from "utils";
+import { http } from "utils/http";
 
 interface AuthForm {
   username: string;
@@ -19,8 +21,23 @@ const AuthContext = createContext<
 // 用于dev-tool显示
 AuthContext.displayName = "AuthContext";
 
+const bootstrapUser = async () => {
+  let user = null;
+  const token = auth.getToken();
+  if (token) {
+    const data = await http("me", { token });
+    user = data.user;
+  }
+
+  return user;
+};
+
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
+
+  useMount(() => {
+    bootstrapUser().then(setUser);
+  });
 
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,41 +1,27 @@
 import { useState, useEffect } from "react";
-import * as qs from "qs";
-
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { cleanObject, useDebounce, useMount } from "utils";
+import { useHttp } from "utils/http";
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: "", // 搜索框的值
     personId: "", // 下拉选项的id
   });
-
   const [users, setUsers] = useState([]); // 存储下拉菜单的用户列表
-
   const [list, setList] = useState([]); // 存储获取的表单数据
-
-  const apiUrl = process.env.REACT_APP_API_URL;
-
   const debouncedParam = useDebounce(param, 200);
+  const client = useHttp();
+
   // 当用户输入或选择下拉框的时候，要请求列表
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
-  }, [debouncedParam, apiUrl]);
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
+  }, [debouncedParam]);
 
   // 初始化users列表
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
