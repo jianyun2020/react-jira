85dfa640918c8baf512ef32adccf249b7ecc585a
jianyun2020 <sujianyun2020@163.com>
2022-07-21 14:01:30 +0800

    style: optimize kanban styles

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -6,7 +6,7 @@ import { useProjects } from "utils/project";
 import { useUsers } from "utils/user";
 import { Button } from "antd";
 import { useProjectModal, useProjectsSearchParams } from "./util";
-import { ErrorBox, Row } from "components/lib";
+import { ErrorBox, Row, ScreenContainer } from "components/lib";
 
 export const ProjectListScreen = () => {
   useDocumentTitle("项目列表", false);
@@ -17,7 +17,7 @@ export const ProjectListScreen = () => {
   const { data: users } = useUsers();
 
   return (
-    <Container>
+    <ScreenContainer>
       <Row marginBottom={2} between={true}>
         <h1>项目列表</h1>
         <Button onClick={openModal}>创建项目</Button>
@@ -30,12 +30,8 @@ export const ProjectListScreen = () => {
         dataSource={list || []}
         users={users || []}
       />
-    </Container>
+    </ScreenContainer>
   );
 };
 
 ProjectListScreen.whyDidYouRender = false;
-
-const Container = styled.div`
-  padding: 3.2rem;
-`;
--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,20 +1,54 @@
-import { Link, Outlet } from "react-router-dom";
+import styled from "@emotion/styled";
+import { Menu } from "antd";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import { EpicScreen } from "screens/epic";
 import { KanbanScreen } from "screens/kanban";
 
+const useRouteType = () => {
+  const units = useLocation().pathname.split("/");
+  return units[units.length - 1];
+};
+
 export const ProjectScreen = () => {
+  const routeType = useRouteType();
+
   return (
-    <div>
-      <h1>ProjectScreen</h1>
-      <Link to={"kanban"}>看板</Link>
-      <Link to={"epic"}>任务组</Link>
-      <Routes>
-        <Route index element={<KanbanScreen />} />
-        <Route path={"kanban"} element={<KanbanScreen />} />
-        <Route path={"epic"} element={<EpicScreen />} />
-      </Routes>
+    <Container>
+      <Aside>
+        <Menu mode="inline" selectedKeys={[routeType]}>
+          <Menu.Item key={"kanban"}>
+            <Link to={"kanban"}>看板</Link>
+          </Menu.Item>
+          <Menu.Item key={"epic"}>
+            <Link to={"epic"}>任务组</Link>
+          </Menu.Item>
+        </Menu>
+      </Aside>
+      <Main>
+        <Routes>
+          <Route index element={<KanbanScreen />} />
+          <Route path={"kanban"} element={<KanbanScreen />} />
+          <Route path={"epic"} element={<EpicScreen />} />
+          <Route index element={<EpicScreen />} />
+        </Routes>
+      </Main>
       <Outlet />
-    </div>
+    </Container>
   );
 };
+
+const Aside = styled.aside`
+  background-color: rgb(244, 245, 247);
+  display: flex;
+`;
+
+const Main = styled.div`
+  box-shadow: -5px 0 5px -5px rgba(0, 0, 0, 0.1);
+  display: flex;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 16rem 1fr;
+`;
